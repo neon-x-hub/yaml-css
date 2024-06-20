7eb05699f9837dcd148b797eb79c6a24c37b9d89
neon-x-hub <abdomem7@gmail.com>
2024-06-20 23:41:47 +0100

    Add support for mixins and variables for SCSS

--- yaml-to-css.js
+++ yaml-to-css.js
@@ -1,17 +1,38 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 
+
 function yamlToScss(yamlObj, depth = 0) {
   let scss = '';
   const indent = '  '.repeat(depth);
-  for (const key in yamlObj) {
-    const value = yamlObj[key];
-    if (typeof value === 'object') {
-      scss += `${indent}${key} {\n${yamlToScss(value, depth + 1)}${indent}}\n`;
-    } else {
-      scss += `${indent}${key}: ${value};\n`;
+  
+  if (yamlObj.variables) {
+    for (const key in yamlObj.variables) {
+      scss += `${indent}$${key}: ${yamlObj.variables[key]};\n`;
     }
   }
+
+  if (yamlObj.mixins) {
+    for (const key in yamlObj.mixins) {
+      scss += `${indent}@mixin ${key} {\n${yamlToScss(yamlObj.mixins[key], depth + 1)}${indent}}\n`;
+    }
+  }
+
+  for (const key in yamlObj) {
+    if (key !== 'variables' && key !== 'mixins') {
+      const value = yamlObj[key];
+      if (typeof value === 'object') {
+        scss += `${indent}${key} {\n${yamlToScss(value, depth + 1)}${indent}}\n`;
+      } else {
+        if (key.startsWith('@include')) {
+          scss += `${indent}@include ${value};\n`;
+        } else {
+          scss += `${indent}${key}: ${value};\n`;
+        }
+      }
+    }
+  }
+  
   return scss;
 }
 
@@ -23,4 +44,4 @@ const yamlObj = yaml.load(yamlFile);
 const scssOutput = yamlToScss(yamlObj);
 fs.writeFileSync(process.argv[3], scssOutput);
 
-console.log('CSS and SCSS files generated successfully!');
+console.log('CSS/SCSS file generated successfully!');
